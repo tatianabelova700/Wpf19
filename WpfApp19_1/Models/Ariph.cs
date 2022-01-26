ce9441a86c767f63b4e30f82f04e2756fad2c7f3
Tatyana <Tatyana@DESKTOP-74DNP3C>
2022-01-26 19:36:46 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19_1/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp19_1.Models
+{
+    class Ariph
+    {
+       public static double Add(double a) => (2*Math.PI)*a;
+    }
+}
---
+++ WpfApp19_1/ViewModels/MainWindowsViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp19_1.Models;
+
+namespace WpfApp19_1.ViewModels
+{
+    class MainWindowsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        private double number2;
+        
+        private double number3;
+        public double Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number3 = Ariph.Add(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            return true;
+        }
+        public MainWindowsViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
